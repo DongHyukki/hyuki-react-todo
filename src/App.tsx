385c55b6d4bc0d4705509fdd99f497e0625734e1
donghyuk.kang <donghyuk.kang@greenlabsfin.com>
2023-03-04 21:01:14 +0900

    feat: Todo List 따라하기 ( 기본 구조 및 init page )

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, CSSProperties} from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component<any, any> {
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  } as CSSProperties
+
+  getStyle(): CSSProperties {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: 'none',
+    }
+  }
+
+  render() {
+    return (
+        <div className="container">
+          <div className="todoBlock">
+            <div className="title">
+              <h1> 할일 목록 </h1>
+            </div>
+            <div style={this.getStyle()}>
+              <input type="checkbox" defaultChecked={false}/>
+              공부하기
+              <button style={this.btnStyle}>x</button>
+            </div>
+          </div>
+        </div>
+    )
+  }
 }
-
-export default App;
